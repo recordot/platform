e332e6f55d8decc8f018c2340e1ab0dce71c53b8
garam <garam-park@naver.com>
2020-11-23 23:54:36 +0900

    환경 변수 등 수정

--- ormconfig.example.ts
+++ ormconfig.example.ts
@@ -3,37 +3,65 @@ import { ConnectionOptions } from "typeorm";
 const entities = [__dirname + "/src/impl/orms/**/*.ts"];
 
 const connectionOptions: ConnectionOptions[] = [
+    // npm test 시 메모리에서 동작하도록...
     {
         name: "test",
-        type: 'sqljs',
-        entities,
-        logging: false,
+        type: "sqljs",
+        logging:true,
+        synchronize: true,
         dropSchema: true, // Isolate each test case
-        synchronize: true
+        entities,
+        migrations: ["src/infras/database/seed/dev/**/*.ts"],
     },
+    // 로컬 환경에서 개발 ...
     {
-        name: "develop",
+        name: "local",
         type: "sqlite",
         database: "db.sqlite3",
+        logging: true,
         synchronize: true,
-        dropSchema: false, // Isolate each test case
-        entities
+        dropSchema: true, // 데이터 초기화
+        entities,
+        migrations: ["src/infras/database/seed/dev/**/*.ts"],
     },
     {
-        name: "default",
-        type: "mariadb",
-        database: 'platform',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: '1',
-        synchronize: true,
-        dropSchema: true,
-        logging: false,
+        name: "stage",
+        type: process.env.DB_TYPE as any,
+        database: process.env.DB_NAME,
+        synchronize: false,
+        logging: true,
+        dropSchema: false,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT as unknown as number,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
         entities,
-        migrations: ["src/infras/database/migration/develop/*.ts"],
+        migrations: [
+            "src/infras/database/migration/**/*.ts",
+            "src/infras/database/seed/stg/**/*.ts"
+        ],
         cli: {
-            migrationsDir: "src/infras/database/migration/develop"
+            migrationsDir: "src/infras/database/migration"
+        }
+    },
+    {
+        name: "production",
+        type: process.env.DB_TYPE as any,
+        database: process.env.DB_NAME,
+        synchronize: false,
+        logging: true,
+        dropSchema: false,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT as unknown as number,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        entities,
+        migrations: [
+            "src/infras/database/migration/**/*.ts",
+            "src/infras/database/seed/prod/**/*.ts"
+        ],
+        cli: {
+            migrationsDir: "src/infras/database/migration"
         }
     }
 ];
